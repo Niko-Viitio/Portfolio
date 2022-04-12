528eb16945948a6df3e0c5bbe274a0a59ac3b2f4
Niko-Viitio <dnivi009@edu.xamk.fi>
2022-04-12 13:15:01 +0300

    fixed header zoom script

--- scripts/headerZoom.js
+++ scripts/headerZoom.js
@@ -1,13 +1,24 @@
 var header = document.getElementsByClassName("header")[0];
 
 setInterval(function() {
+  //Get y offset for zooming the background
   var offset = getYPosition();
   var sizeMult = 100 + offset / 20;
   if (sizeMult > 120) sizeMult = 120;
 
+  //Get window width for scaling the background
+  var windowWidth = window.innerWidth;
+  var widthMult = (1920 / windowWidth) * 0.4;
+  if (widthMult < 1) widthMult = 1;
+  else if (widthMult > 1.5) widthMult = 1.5;
+
+  //console.log("Width mult: " + widthMult);
+  sizeMult *= widthMult;
+
+  //Apply zoom multiplier to the background
   var sizeStr = sizeMult.toString() + "%";
   header.style.backgroundSize = sizeStr;
-  console.log("Size: " + sizeStr);
+  //console.log("Screen width: " + windowWidth + " Size: " + sizeStr);
 }, 10);
 
 function getYPosition() {
